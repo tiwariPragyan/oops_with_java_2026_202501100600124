2bb74b1c3dacd92937da0ed2e7851ebba3a9b28d
Pragyan Tiwari <tiwaripragyan88@gmail.com>
2026-09-03 11:46:49 +0530

    Inheritance example

---
+++ inheritance.java
@@ -0,0 +1,46 @@
+package Learning_JAVA;
+class Vehicle{
+    private String name;
+    private int speed;
+    public void display(){
+        System.out.println("Vehicle " + name + " " + "is moving at "+ speed + " "+ "km/h");
+    }
+    public void move(){
+        System.out.println("Vehicle can move");
+    }
+    public Vehicle(String n, int s){
+        name = n;
+        speed = s;
+    }
+}
+
+class Bike extends Vehicle{
+    public void wheels(){
+        System.out.println("Bike has 2 wheels");
+    }
+    public Bike(String name, int speed){
+        super(name,speed);
+    }
+}
+
+class Car extends Vehicle{
+    public void wheels(){
+        System.out.println("Car har 4 wheels");
+    }
+    public Car(String name, int speed){
+        super(name,speed);
+    }
+}
+
+public class inheritance {
+    public static void main(String[] args) {
+        Vehicle obj1 = new Bike("Splendor", 60);
+        //Upcasting;
+        Vehicle obj2 = new Car("BMW", 200);
+        Vehicle[] obj = {obj1,obj2};
+        for(Vehicle i: obj){
+            i.move();
+            i.display();
+        }
+    }
+}
