baaa025bb403216788e8353ee969c113f9c7f336
Pragyan Tiwari <tiwaripragyan88@gmail.com>
2026-09-11 13:04:32 +0530

    Pushed

--- ExceptionHandling/PredefinedException.java
+++ ExceptionHandling/PredefinedException.java
@@ -1,5 +1,6 @@
 package oops_with_java_2026_202501100600124.ExceptionHandling;
 
+import java.io.FileReader;
 import java.util.Scanner;
 
 public class PredefinedException {
@@ -8,11 +9,15 @@ public class PredefinedException {
         int arr[] = {1,2,3};
         int i = sc.nextInt();
         try{
+            FileReader fr = new FileReader("abc.txt");
             System.out.println(arr[i]);
             int a = sc.nextInt();
             System.out.println(arr[i]/a);
-        }catch(Exception e){
-            System.out.println("Exception caught is "+ e);
+        }catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("Exception caught is "+ e.getMessage());
+        }
+        catch(Exception e){  // Exception is parent class of all exceptions. It can handle all type  of exceptions
+            System.out.println("Exception caught is "+ e.getMessage());
         }
         finally{
             System.out.println("Finally Executed");
